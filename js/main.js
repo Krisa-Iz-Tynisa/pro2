084e035b01cb4c1a269f76531eea76e7335bb602
Krisa-Iz-Tynisa <mentalgeek4@gmail.com>
2021-05-29 20:53:06 +0300

    rere

--- js/main.js
+++ js/main.js
@@ -3,8 +3,29 @@ new Swiper ('.swiper-container',{
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
-    slidesPerView: 2,
+    // slidesPerView: 2,
     spaceBetween: 30,
+    loop: true,
+    breakpoints: {
+        767: {
+            freeMode: true,
+            slidesPerView: 1,
+            allowTouchMove: true,
+            scrollbar: {
+                el: '.scroll-swiper-scrollbar',
+                hide: true,
+            },
+        },
+        1024: { 
+            allowTouchMove: true,
+        },
+        1260: {
+            allowTouchMove: true,
+            spaceBetween: 30,
+            slidesPerView: 2,
+            
+        }
+    }
 
 
 });
